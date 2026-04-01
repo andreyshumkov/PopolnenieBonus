20d42432fa4f464f2a8e378db76c0ecdaabe2143
Drews <andrey.shumkov228@gmail.com>
2026-04-02 02:47:38 +0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,11 @@ public static void main(String[] args) {
         percent = 0;
     }
 
-    int itogbalance = balance + deposit + percent ;// бонус в деп
+    int itogbalance = balance + deposit + percent;// бонус в депозит
     int bonus = 0;
     if (deposit > 1000)
-        bonus=deposit  / 100 ;
+        bonus = deposit / 100;
 
     System.out.println("Балансик: " + itogbalance);
-    System.out.println("Бонусные рубли " + bonus );
+    System.out.println("Бонусные рубли " + bonus);
 }
-// Объявляете переменные для входных данных и
-// параметров программы: начального счёта,
-// суммы пополнения и тп
-
-// Условным оператором проверяете, превысила ли
-// сумма пополнения порог, и для этих двух разных
-// сценариев рассчитываете сумму бонуса и выводите
-// на экран.
